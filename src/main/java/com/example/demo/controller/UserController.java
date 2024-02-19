c57cd4720e0314a8d4d2ea10bec5732159b66ad6
ascaryaaa <allaamascarya@gmail.com>
2024-02-19 16:04:56 +0700

    Implemented createUser endpoint in UserController.

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+
+@RestController //gabungan dari 2 anotasi, antara Controller dan ResponseBody
+@RequestMapping("/users")
+public class UserController {
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    public User creatUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+}
