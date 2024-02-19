99a3059566f778d08d9f96cfa059991432502377
ascaryaaa <allaamascarya@gmail.com>
2024-02-19 14:46:14 +0700

    Added user registration and retrieval methods~

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,25 @@
 package com.example.demo.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.User;
+
+@Repository
 public class UserRepository {
+    private final List<User> users = new ArrayList<>();
+
+    public User sayUser(User user) {
+        user.setId((long) users.size() + 1);
+        users.add(user);
+        return user;
+        
+    }
+
+    public List<User> findById(Long id) {
+        return users;
+    }
     
 }
