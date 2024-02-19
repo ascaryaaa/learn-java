243c76d19fa174615ce0af624b9d481ed452ee10
ascaryaaa <allaamascarya@gmail.com>
2024-02-19 14:36:40 +0700

    add getter, setter, constructor, override/implement methods using source action

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,42 @@
+package com.example.demo.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String email;
+    public long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+    
+    
+}
