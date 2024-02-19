ee95cb61bfca5edda3165bce5a206ddd685b3f78
ascaryaaa <allaamascarya@gmail.com>
2024-02-19 16:10:31 +0700

    Added createUser and getUserById endpoints in UserController

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +22,14 @@ public class UserController {
     public UserController(UserService service) {
         this.service = service;
     }
-
+    
+    @PostMapping
     public User creatUser(@RequestBody User user) {
         return service.saveUser(user);
+    }
+
+    @GetMapping("/id")
+    public List<User> getUserById(@PathVariable Long id) {
+        return service.getById(id);
     }
 }
